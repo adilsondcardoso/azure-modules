fd8589aa55a30cb452aaffda7a2c5ab77133a020
Adilson Dias Cardoso <addtech.adcardoso@terceiros.mongeralaegon.com.br>
2018-11-01 22:34:25 -0300

    correção variável de saída

--- azure-sqldatabase/output.tf
+++ azure-sqldatabase/output.tf
@@ -3,5 +3,5 @@ output "database_name" {
 }
 output "connection_string" {
   description = "Connection string para o bando de dados SQL criado no Azure."
-  value       = "Server=tcp:${azurerm_sql_server.db.server_name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=username;Password=password;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value       = "Server=tcp:${azurerm_sql_server.db.*.server_name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=username;Password=password;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }

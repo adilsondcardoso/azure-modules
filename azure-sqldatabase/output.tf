9d545991038cc594100f9c44d8544b28abd58312
Adilson Dias Cardoso <addtech.adcardoso@terceiros.mongeralaegon.com.br>
2018-11-01 22:44:38 -0300

    Correção arquivo saída sqlDataBase

--- azure-sqldatabase/output.tf
+++ azure-sqldatabase/output.tf
@@ -1,7 +1,7 @@
-output "database_name" {
+output "db_name" {
   value = "${azurerm_sql_database.db.*.name}"
 }
-output "connection_string" {
+output "db_connectionString" {
   description = "Connection string para o bando de dados SQL criado no Azure."
-  value       = "Server=tcp:${azurerm_sql_server.db.*.server_name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.db.name};Persist Security Info=False;User ID=username;Password=password;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value       = ["Server=tcp:${azurerm_sql_database.db.*.server_name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.db.*.name};Persist Security Info=False;User ID=username;Password=password;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"]
 }

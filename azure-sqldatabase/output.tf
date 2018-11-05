e5b81bc896e5713f131274c2b6b60e323d4606a3
Adilson Dias Cardoso <addtech.adcardoso@terceiros.mongeralaegon.com.br>
2018-11-05 09:24:29 -0300

    Ajuste output sqldatabase

--- azure-sqldatabase/output.tf
+++ azure-sqldatabase/output.tf
@@ -1,7 +1,3 @@
 output "db_name" {
   value = "${azurerm_sql_database.db.*.name}"
 }
-output "db_connectionString" {
-  description = "Connection string para o bando de dados SQL criado no Azure."
-  value       = "Server=tcp:${azurerm_sql_database.db.*.server_name}.database.windows.net,1433;Initial Catalog=${azurerm_sql_database.db.*.name};Persist Security Info=False;User ID=username;Password=password;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-}
